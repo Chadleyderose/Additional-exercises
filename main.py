d79b59f7b3e640574432dca4ed7e6667072b0223
Chadleyderose <68062582+Chadleyderose@users.noreply.github.com>
2020-10-13 21:11:02 +0200

    Add files via upload
    new file

--- main.py
+++ main.py
@@ -7,20 +7,37 @@ root.title("Temperture convertor"
 labelframe_c = LabelFrame(root, text = "Celcuis to Fahrehheit" , padx=48 , pady=48)
 labelframe_c.pack(fill="both")
 labelframe_c .place(x = 20 , y = 10)
-entry_c = Entry(labelframe_c)
+entry_c = Entry(labelframe_c , state = 'disable')
 entry_c.pack()
 
 labelframe_f = LabelFrame(root, text = "Fahrehheit to Celcuis " , padx=48 , pady=48)
 labelframe_f.pack(fill="both")
 labelframe_f .place(x = 300 , y = 10)
-entry_f = Entry(labelframe_f)
+entry_f = Entry(labelframe_f , state = 'disable')
 entry_f.pack()
 
-button_c =Button(root, text = "Activate-Celcuis to Fahrehheit" )
+def active ():
+    entry_c.config(state ='normal')
+
+button_c =Button(root, text = "Activate-Celcuis to Fahrehheit " , command= active  )
 button_c .place(x = 20 , y =180 )
 
-button_f =Button(root, text = "Activate-Fahrehheit to Celcuis" )
+def active ():
+    entry_f.config(state ='normal')
+
+button_f =Button(root, text = "Activate-Fahrehheit to Celcuis" , command= active)
 button_f .place(x = 300 , y =180 )
+
+def convert():
+    intvar(entry_c)
+    intvar (entry_f)
+    if int:
+       int = entry_c.get()
+       btn_Caculate.set((int * 9.0 / 5) + 32)
+def convert():
+    if int:
+         int=entry_f.get()
+         btn_Caculate.set((int - 32) * 5/9)
 
 btn_Caculate =Button(root, text ="Calculate Conversion")
 btn_Caculate .pack()
@@ -30,8 +47,18 @@ btn_result=Entry (root, bg ="green2" ,)
 btn_result.pack()
 btn_result.place(x =200 , y =250 )
 
-btn_clear =Button(root, text ="clear")
+def clear():
+     entry_f.delete(0,END)
+     entry_c.delete(0,END)
+     btn_result.delete(0,END)
+btn_clear =Button(root, text ="Clear" , command=clear)
 btn_clear .pack()
-btn_clear .place(x = 100 , y =300 )
+btn_clear .place(x = 400 , y =250 )
+
+def quit():
+    root.destroy()
+btn_quit =Button(root, text = "Exit Program" , command=quit)
+btn_quit .pack()
+btn_quit .place(x = 380 , y =290 )
 
 root.mainloop()
